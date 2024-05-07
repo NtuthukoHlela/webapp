b5a4de0f975710aacf57760a73c9bfcb5239131e
Ntuthuko Hlela <111696810+NtuthukoHlela@users.noreply.github.com>
2024-05-07 23:32:42 +0200

    Fixind the width

--- src/Home.js
+++ src/Home.js
@@ -2,17 +2,10 @@ import { Helmet } from "react-helmet"
 import { Link } from "react-router-dom"
 
 
-let generalStyle = {
-    color: "black",
-    padding: 24,
-    fontFamily: "Sans-Serif",
-    textAlign: "center"
-  }
-let x = window.innerWidth
 
-let widthx = x/20
-
-
+  let x = window.innerWidth
+  let widthx = x/20
+  
 export default function Home () {
     return (
 <>
@@ -20,20 +13,24 @@ export default function Home () {
   <title>Home</title>
   </Helmet>   
 
-<h1 style= {generalStyle}> <b> Home Page </b> </h1>
-<h1> width = {x} and new padding - width is = {widthx} </h1>
-<li>
-<Link to='/Competency'> Competency </Link>
-</li>
+<h3 style={{textAlign: "center", paddingRight: widthx, paddingLeft: widthx, fontFamily: 'Fira Sans'}}> 
+Ntuthuko Hlela 
+</h3>
+<h4 style={{textAlign: "justify", paddingRight: widthx, paddingLeft: widthx, fontFamily: 'Fira Sans'}}> 
+
+      I like being at the forefront of research, being deeply engaged in meaningful and practical work makes me happy!
+        Therefore, this website will mainly focus on research or tech tools that I find interesting.
+        I also like life hacks that make me more productive, effective and efficient, so, I will also be writing about these.
+         <br/> </h4>
+
+        <hr/>
+
+
+<h4 style={{paddingLeft: widthx, fontFamily: 'Fira Sans'}}> 1.
+<Link to='/Competency' > Competency </Link>
+</h4>
 
 <div>
-  <h1>This is a test</h1>
-  <h1> {widthx}</h1>
-
-  <p style={{paddingRight: widthx, paddingLeft: widthx, textAlign: 'justify'}}>
-  This article aims to outline concrete ways of approaching the road to competency. 
-I have always been curious, knowing more excites me, I see competency as the ultimate success. Knowing how to do something thoroughly and expertly is not only a positive locally (people pay you), but it is also beneficial at a societal level (you solve real problems). This raises the question of, how one becomes competent. Below, I list routines/techniques/practices that make learning/working easier/faster, at least for me.   
-  </p>
 
 </div>
 </>
--- src/competency.js
+++ src/competency.js
@@ -7,6 +7,9 @@ let generalStyle = {
     textAlign: "center"
   }
 
+let x = window.innerWidth
+let widthx = x/20
+
 export default function competencyPage () {
 
     return (
@@ -19,7 +22,7 @@ export default function competencyPage () {
 
 <h1 style= {generalStyle}> <b> Becoming competent </b> </h1>
 
-<div style={{textAlign: "justify", paddingRight:200, paddingLeft: 200}}>
+<div style={{textAlign: "justify", paddingRight:widthx, paddingLeft: widthx}}>
 
  <p>  This article aims to outline concrete ways of approaching the road to competency. 
 I have always been curious, knowing more excites me, I see competency as the ultimate success. Knowing how to do something thoroughly and expertly is not only a positive locally (people pay you), but it is also beneficial at a societal level (you solve real problems). This raises the question of, how one becomes competent. Below, I list routines/techniques/practices that make learning/working easier/faster, at least for me.   
